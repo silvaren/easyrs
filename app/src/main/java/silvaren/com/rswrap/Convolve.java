f980710b34f77c9326f1d9b5103056889fc0974b
Renato Oliveira <renato.hawk@gmail.com>
2016-07-09 01:42:00 -0300

    Adding convolve 3x3

--- app/src/main/java/silvaren/com/rswrap/Convolve.java
+++ app/src/main/java/silvaren/com/rswrap/Convolve.java
@@ -3,6 +3,7 @@ package silvaren.com.rswrap;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.ScriptIntrinsicConvolve3x3;
 import android.support.v8.renderscript.ScriptIntrinsicConvolve5x5;
 
 class Convolve {
@@ -34,6 +35,32 @@ class Convolve {
         Allocation aout = Allocation.createTyped(bitmapRSContext.rs, bitmapRSContext.ain.getType());
 
         ScriptIntrinsicConvolve5x5 convolve5x5Script = ScriptIntrinsicConvolve5x5.create(
+                bitmapRSContext.rs, bitmapRSContext.ain.getElement());
+        convolve5x5Script.setInput(bitmapRSContext.ain);
+        convolve5x5Script.setCoefficients(coefficients);
+        convolve5x5Script.forEach(aout);
+
+        aout.copyTo(outputBitmap);
+    }
+
+    public static void convolve3x3InPlace(Context context, Bitmap bitmap, float[] coefficients) {
+        doConvolve5x5(context, bitmap, bitmap, coefficients);
+    }
+
+    public static Bitmap convolve3x3(Context context, Bitmap inputBitmap, float[] coefficients) {
+        Bitmap.Config config = inputBitmap.getConfig();
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap.getWidth(), inputBitmap.getHeight(),
+                config);
+        doConvolve3x3(context, inputBitmap, outputBitmap, coefficients);
+        return outputBitmap;
+    }
+
+    private static void doConvolve3x3(Context context, Bitmap inputBitmap, Bitmap outputBitmap,
+                                      float[] coefficients) {
+        BitmapRSContext bitmapRSContext = BitmapRSContext.createFromBitmap(inputBitmap, context);
+        Allocation aout = Allocation.createTyped(bitmapRSContext.rs, bitmapRSContext.ain.getType());
+
+        ScriptIntrinsicConvolve3x3 convolve5x5Script = ScriptIntrinsicConvolve3x3.create(
                 bitmapRSContext.rs, bitmapRSContext.ain.getElement());
         convolve5x5Script.setInput(bitmapRSContext.ain);
         convolve5x5Script.setCoefficients(coefficients);
