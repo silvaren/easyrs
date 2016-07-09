f670a8cd61dcd6254f0399c02b9a895c18aa81e1
Renato Oliveira <renato.hawk@gmail.com>
2016-07-09 03:23:50 -0300

    Adding utils class

--- app/src/main/java/silvaren/com/rswrap/MainActivity.java
+++ app/src/main/java/silvaren/com/rswrap/MainActivity.java
@@ -34,53 +34,14 @@ public class MainActivity extends AppCompatActivity {
 //        Bitmap convolved5x5Bitmap = Convolve.convolve5x5(this, sampleBitmap, Convolve.Kernels5x5.SOBEL_X);
 //        Bitmap colorBitmap = drawColorBitmap(sampleBitmap, 0xFFFF0000);
 //        int[] histograms = Histogram.rgbaHistograms(this, colorBitmap);
-        int[] histogram = Histogram.luminanceHistogram(this, sampleBitmap);
+//        int[] histogram = Histogram.luminanceHistogram(this, sampleBitmap);
 //        Bitmap histogramsBitmap = drawHistograms(histograms, 4);
-        Bitmap histogramBitmap = drawHistograms(histogram, 1);
+//        Bitmap histogramBitmap = drawHistograms(histogram, 1);
+
 
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setImageBitmap(histogramBitmap);
-    }
-
-    private Bitmap drawColorBitmap(Bitmap sampleBitmap, int color) {
-        Bitmap outputBitmap = Bitmap.createBitmap(Constants.COLOR_DEPTH, Constants.COLOR_DEPTH,
-                Bitmap.Config.ARGB_8888);
-        for (int x = 0; x < Constants.COLOR_DEPTH; x++) {
-            for (int y = 0; y < Constants.COLOR_DEPTH; y++) {
-                outputBitmap.setPixel(x, y, color);
-            }
-        }
-
-        return outputBitmap;
-    }
-
-    private Bitmap drawHistograms(int[] histograms, int channels) {
-        Bitmap outputBitmap = Bitmap.createBitmap(Constants.COLOR_DEPTH * channels,
-                Constants.COLOR_DEPTH, Bitmap.Config.ARGB_8888);
-
-        float[] maxes = new float[channels];
-        for (int c = 0; c < channels; c++) {
-            int max = 0;
-            for (int i = 0; i < Constants.COLOR_DEPTH; i++) {
-                max = Math.max(histograms[c + i * channels], max);
-            }
-            maxes[c] = max;
-        }
-
-
-        for (int x = 0; x < Constants.COLOR_DEPTH * channels; x++) {
-            for (int y = 0; y < Constants.COLOR_DEPTH; y++) {
-                int c = x / Constants.COLOR_DEPTH;
-                int i = x % Constants.COLOR_DEPTH;
-                int height = Constants.COLOR_DEPTH - 1 - (int) (histograms[c + i * channels] *
-                        (Constants.COLOR_DEPTH - 1.f) / maxes[c]);
-                int color = y > height? 0xFF000000 : 0xFFFFFFFF;
-                outputBitmap.setPixel(x, y, color);
-            }
-        }
-
-        return outputBitmap;
+        imageView.setImageBitmap(sampleBitmap);
     }
 
     private Bitmap resize(Context context, Bitmap inputBitmap, int width, int height) {
---
+++ app/src/main/java/silvaren/com/rswrap/Utils.java
@@ -0,0 +1,46 @@
+package silvaren.com.rswrap;
+
+import android.graphics.Bitmap;
+
+public class Utils {
+
+    private Bitmap drawColorBitmap(Bitmap sampleBitmap, int color) {
+        Bitmap outputBitmap = Bitmap.createBitmap(Constants.COLOR_DEPTH, Constants.COLOR_DEPTH,
+                Bitmap.Config.ARGB_8888);
+        for (int x = 0; x < Constants.COLOR_DEPTH; x++) {
+            for (int y = 0; y < Constants.COLOR_DEPTH; y++) {
+                outputBitmap.setPixel(x, y, color);
+            }
+        }
+
+        return outputBitmap;
+    }
+
+    private Bitmap drawHistograms(int[] histograms, int channels) {
+        Bitmap outputBitmap = Bitmap.createBitmap(Constants.COLOR_DEPTH * channels,
+                Constants.COLOR_DEPTH, Bitmap.Config.ARGB_8888);
+
+        float[] maxes = new float[channels];
+        for (int c = 0; c < channels; c++) {
+            int max = 0;
+            for (int i = 0; i < Constants.COLOR_DEPTH; i++) {
+                max = Math.max(histograms[c + i * channels], max);
+            }
+            maxes[c] = max;
+        }
+
+
+        for (int x = 0; x < Constants.COLOR_DEPTH * channels; x++) {
+            for (int y = 0; y < Constants.COLOR_DEPTH; y++) {
+                int c = x / Constants.COLOR_DEPTH;
+                int i = x % Constants.COLOR_DEPTH;
+                int height = Constants.COLOR_DEPTH - 1 - (int) (histograms[c + i * channels] *
+                        (Constants.COLOR_DEPTH - 1.f) / maxes[c]);
+                int color = y > height? 0xFF000000 : 0xFFFFFFFF;
+                outputBitmap.setPixel(x, y, color);
+            }
+        }
+
+        return outputBitmap;
+    }
+}
