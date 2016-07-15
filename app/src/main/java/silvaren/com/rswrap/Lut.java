e087e75892dfc69b763b448f12f606c9473bd47c
Renato Oliveira <renato.hawk@gmail.com>
2016-07-15 00:17:32 -0300

    Extracting lut class

---
+++ app/src/main/java/silvaren/com/rswrap/Lut.java
@@ -0,0 +1,29 @@
+package silvaren.com.rswrap;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.ScriptIntrinsicLUT;
+
+class Lut {
+    public static Bitmap negativeEffect(Context context, Bitmap inputBitmap) {
+        BitmapRSContext bitmapRSContext = BitmapRSContext.createFromBitmap(inputBitmap, context);
+        Bitmap.Config config = inputBitmap.getConfig();
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap.getWidth(), inputBitmap.getHeight(),
+                config);
+        Allocation aout = Allocation.createTyped(bitmapRSContext.rs, bitmapRSContext.ain.getType());
+
+        ScriptIntrinsicLUT lutScript = ScriptIntrinsicLUT.create(bitmapRSContext.rs, bitmapRSContext.ain.getElement());
+        for (int i = 0; i < 256; i++) {
+            lutScript.setAlpha(i, i);
+            lutScript.setRed(i, 255 - i);
+            lutScript.setGreen(i, 255 - i);
+            lutScript.setBlue(i, 255 - i);
+        }
+
+        lutScript.forEach(bitmapRSContext.ain, aout);
+
+        aout.copyTo(outputBitmap);
+        return outputBitmap;
+    }
+}
--- app/src/main/java/silvaren/com/rswrap/MainActivity.java
+++ app/src/main/java/silvaren/com/rswrap/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.ScriptIntrinsicLUT;
 import android.support.v8.renderscript.ScriptIntrinsicResize;
 import android.support.v8.renderscript.Type;
 import android.widget.ImageView;
@@ -45,29 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageBitmap(mappedBitmap);
-    }
-
-    static class Lut {
-        public static Bitmap negativeEffect(Context context, Bitmap inputBitmap) {
-            BitmapRSContext bitmapRSContext = BitmapRSContext.createFromBitmap(inputBitmap, context);
-            Bitmap.Config config = inputBitmap.getConfig();
-            Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap.getWidth(), inputBitmap.getHeight(),
-                    config);
-            Allocation aout = Allocation.createTyped(bitmapRSContext.rs, bitmapRSContext.ain.getType());
-
-            ScriptIntrinsicLUT lutScript = ScriptIntrinsicLUT.create(bitmapRSContext.rs, bitmapRSContext.ain.getElement());
-            for (int i = 0; i < 256; i++) {
-                lutScript.setAlpha(i, i);
-                lutScript.setRed(i, 255 - i);
-                lutScript.setGreen(i, 255 - i);
-                lutScript.setBlue(i, 255 - i);
-            }
-
-            lutScript.forEach(bitmapRSContext.ain, aout);
-
-            aout.copyTo(outputBitmap);
-            return outputBitmap;
-        }
     }
 
     static class Resize {
