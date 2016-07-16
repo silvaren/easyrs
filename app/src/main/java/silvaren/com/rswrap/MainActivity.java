73c16ff55b9cb76ea0361cd707c4ca7f6c998823
Renato Oliveira <renato.hawk@gmail.com>
2016-07-16 01:51:32 -0300

    Adding 3d lut script

--- app/src/main/java/silvaren/com/rswrap/MainActivity.java
+++ app/src/main/java/silvaren/com/rswrap/MainActivity.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsic3DLUT;
 import android.support.v8.renderscript.ScriptIntrinsicResize;
 import android.support.v8.renderscript.Type;
 import android.widget.ImageView;
@@ -40,10 +43,41 @@ public class MainActivity extends AppCompatActivity {
 //        Nv21Image nv21Image = Nv21Image.generateSample();
 //        Bitmap outputBitmap = YuvToRgb.yuvToRgb(this, nv21Image);
 //        ColorMatrix.convertToGrayscaleInPlace(this, sampleBitmap);
-        Bitmap mappedBitmap = Lut.negativeEffect(this, sampleBitmap);
+//        Bitmap mappedBitmap = Lut.negativeEffect(this, sampleBitmap);
+        do3dLut(this, sampleBitmap);
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setImageBitmap(mappedBitmap);
+        imageView.setImageBitmap(sampleBitmap);
+    }
+
+    private static Allocation initSampleCube(RenderScript rs) {
+        final int sx = 2;
+        final int sy = 2;
+        final int sz = 2;
+        Type.Builder tb = new Type.Builder(rs, Element.U8_4(rs));
+        tb.setX(sx);
+        tb.setY(sy);
+        tb.setZ(sz);
+        Type t = tb.create();
+        Allocation cube = Allocation.createTyped(rs, t);
+        int dat[] = new int[sx * sy * sz];
+        dat[0] = 0xffffffff;
+        dat[7] = 0xff000000;
+        cube.copyFromUnchecked(dat);
+
+        return cube;
+    }
+
+    public static void do3dLut(Context context, Bitmap inputBitmap) {
+        BitmapRSContext bitmapRSContext = BitmapRSContext.createFromBitmap(inputBitmap, context);
+        Allocation aout = Allocation.createTyped(bitmapRSContext.rs, bitmapRSContext.ain.getType());
+
+        ScriptIntrinsic3DLUT script3dLut = ScriptIntrinsic3DLUT.create(
+                bitmapRSContext.rs, bitmapRSContext.ain.getElement());
+        Allocation lut = initSampleCube(bitmapRSContext.rs);
+        script3dLut.setLUT(lut);
+        script3dLut.forEach(bitmapRSContext.ain, aout);
+        aout.copyTo(inputBitmap);
     }
 
     static class Resize {
