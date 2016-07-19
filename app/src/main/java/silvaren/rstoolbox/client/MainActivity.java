a58c4b0d002714b12a5d9288b4c7fc06c21dd7ee
Renato Oliveira <renato.hawk@gmail.com>
2016-07-19 01:54:29 -0300

    Adding nv21 script poc

--- app/src/main/java/silvaren/rstoolbox/client/MainActivity.java
+++ app/src/main/java/silvaren/rstoolbox/client/MainActivity.java
@@ -4,6 +4,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.support.v8.renderscript.Type;
 import android.widget.ImageView;
 
 import silvaren.rstoolbox.tools.Nv21Image;
@@ -26,9 +31,6 @@ public class MainActivity extends AppCompatActivity {
         Bitmap sampleBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample, options);
         Bitmap sampleEdgeBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample_edge, options);
 
-        Nv21Image nv21Image = Nv21Image.convertToNV21(this, sampleBitmap);
-
-        Bitmap outBitmap = Nv21Image.nv21ToBitmap(sampleBitmap, nv21Image.nv21ByteArray);
 
 //        Bitmap blurredBitmap = blur(sampleBitmap, 25.f, this);
 //        Bitmap resizedBitmap = resize(this, sampleBitmap, 50, 50);
@@ -44,7 +46,27 @@ public class MainActivity extends AppCompatActivity {
 //        ColorMatrix.convertToGrayscaleInPlace(this, sampleBitmap);
 //        Bitmap mappedBitmap = Lut.negativeEffect(this, sampleBitmap);
 //        Lut3D.do3dLut(this, sampleBitmap);
+        Nv21Image nv21Image = Nv21Image.convertToNV21(this, sampleBitmap);
 
+        RenderScript rs = RenderScript.create(this);
+        Type.Builder tb = new Type.Builder(rs, Element.createPixel(rs,
+                Element.DataType.UNSIGNED_8, Element.DataKind.PIXEL_YUV));
+        tb.setX(nv21Image.width);
+        tb.setY(nv21Image.height);
+        tb.setYuvFormat(android.graphics.ImageFormat.NV21);
+        Allocation yuvAllocation = Allocation.createTyped(rs, tb.create(), Allocation.USAGE_SCRIPT);
+        yuvAllocation.copyFrom(nv21Image.nv21ByteArray);
+
+        Allocation aout = Allocation.createTyped(rs, yuvAllocation.getType());
+
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(
+                rs,
+                yuvAllocation.getElement());
+        blurScript.setInput(yuvAllocation);
+        blurScript.setRadius(25.f);
+        blurScript.forEach(aout);
+        aout.copyTo(nv21Image.nv21ByteArray);
+        Bitmap outBitmap = Nv21Image.nv21ToBitmap(sampleBitmap, nv21Image.nv21ByteArray);
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageBitmap(outBitmap);
