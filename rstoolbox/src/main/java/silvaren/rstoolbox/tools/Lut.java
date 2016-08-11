37522c576ba2701b5af06e752f39bd77eefcecda
Renato Oliveira <renato.hawk@gmail.com>
2016-08-11 01:39:54 -0300

    Refactoring Lut to use convertingtool

--- rstoolbox/src/main/java/silvaren/rstoolbox/tools/Lut.java
+++ rstoolbox/src/main/java/silvaren/rstoolbox/tools/Lut.java
@@ -2,34 +2,67 @@ package silvaren.rstoolbox.tools;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.ScriptIntrinsicColorMatrix;
 import android.support.v8.renderscript.ScriptIntrinsicLUT;
 
 public class Lut {
-    public static Bitmap negativeEffect(Context context, Bitmap inputBitmap) {
-        RSToolboxContext bitmapRSContext = RSToolboxContext.createFromBitmap(context, inputBitmap);
-        Bitmap.Config config = inputBitmap.getConfig();
-        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap.getWidth(), inputBitmap.getHeight(),
-                config);
-        Allocation aout = Allocation.createTyped(bitmapRSContext.rs, bitmapRSContext.ain.getType());
 
-        ScriptIntrinsicLUT lutScript = ScriptIntrinsicLUT.create(bitmapRSContext.rs, bitmapRSContext.ain.getElement());
-        for (int i = 0; i < 256; i++) {
-            lutScript.setAlpha(i, i);
-            lutScript.setRed(i, 255 - i);
-            lutScript.setGreen(i, 255 - i);
-            lutScript.setBlue(i, 255 - i);
+    private static BaseTool.BaseToolScript lutToolScript = new BaseTool.BaseToolScript<LutParams>() {
+        @Override
+        public void runScript(RSToolboxContext rsToolboxContext, Allocation aout, LutParams scriptParams) {
+            ScriptIntrinsicLUT lutScript = ScriptIntrinsicLUT.create(rsToolboxContext.rs,
+                    rsToolboxContext.ain.getElement());
+            scriptParams.setLutParams(lutScript);
+            lutScript.forEach(rsToolboxContext.ain, aout);
         }
+    };
 
-        lutScript.forEach(bitmapRSContext.ain, aout);
+    @NonNull
+    private static ConvertingTool<LutParams> createConvertingTool() {
+        BaseTool<LutParams> baseTool = new BaseTool<>(lutToolScript);
+        return new ConvertingTool<>(baseTool);
+    }
 
-        aout.copyTo(outputBitmap);
-        return outputBitmap;
+    public static Bitmap negativeEffect(Context context, Bitmap inputBitmap) {
+        ConvertingTool<LutParams> lutTool = createConvertingTool();
+        return lutTool.baseTool.doComputation(context, inputBitmap,
+                new LutParams(LutParams.Operation.NEGATIVE));
     }
 
     public static byte[] negativeEffect(Context context, byte[] nv21ByteArray, int width, int height) {
-        Bitmap srcBitmap = Nv21Image.nv21ToBitmap(nv21ByteArray, width, height);
-        Bitmap outputBmp = negativeEffect(context, srcBitmap);
-        return Nv21Image.convertToNV21(context, outputBmp).nv21ByteArray;
+        ConvertingTool<LutParams> lutTool = createConvertingTool();
+        return lutTool.doComputation(context, nv21ByteArray, width, height,
+                new LutParams(LutParams.Operation.NEGATIVE));
+    }
+
+    private static class LutParams {
+
+        enum Operation {
+            NEGATIVE
+        }
+
+        private final Operation op;
+
+        LutParams(Operation op) {
+            this.op = op;
+        }
+
+        public void setLutParams(ScriptIntrinsicLUT lutScript) {
+            switch (op) {
+                case NEGATIVE: setNegativeEffect(lutScript);
+                    break;
+            }
+        }
+
+        private void setNegativeEffect(ScriptIntrinsicLUT lutScript) {
+            for (int i = 0; i < 256; i++) {
+                lutScript.setAlpha(i, i);
+                lutScript.setRed(i, 255 - i);
+                lutScript.setGreen(i, 255 - i);
+                lutScript.setBlue(i, 255 - i);
+            }
+        }
     }
 }
