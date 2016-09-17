c4c3b93c502bd84e85da6d2dbab1d4f619d65977
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-09-17 20:39:20 -0300

    Adding javadoc to LUT

--- easyrs/src/main/java/io/github/silvaren/easyrs/tools/Lut.java
+++ easyrs/src/main/java/io/github/silvaren/easyrs/tools/Lut.java
@@ -21,12 +21,23 @@ public class Lut {
         }
     };
 
+    /**
+     * Apply a RGBA Look-Up table to a Bitmap image.
+     * @param rgbaLut the RGBA look-up table to apply. See {@link io.github.silvaren.easyrs.tools.params.LutParams.RGBALut}.
+     */
     public static Bitmap applyLut(RenderScript rs, Bitmap inputBitmap, LutParams.RGBALut rgbaLut) {
         ConvertingTool<LutParams> lutTool = new ConvertingTool<>(lutToolScript);
         return lutTool.doComputation(rs, inputBitmap,
                 new LutParams(rgbaLut));
     }
 
+    /**
+     * Apply a RGBA Look-Up table to a NV21 image.
+     * @param nv21ByteArray the original NV21 byte array.
+     * @param width the original NV21 image width.
+     * @param height the original NV21 image height.
+     * @param rgbaLut the RGBA look-up table to apply. See {@link io.github.silvaren.easyrs.tools.params.LutParams.RGBALut}.
+     */
     public static byte[] applyLut(RenderScript rs, byte[] nv21ByteArray, int width,
                                         int height, LutParams.RGBALut rgbaLut) {
         ConvertingTool<LutParams> lutTool = new ConvertingTool<>(lutToolScript);
